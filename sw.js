2c3e51e2f7f031d6c26d14c3293fcb846fc70369
AlbBris <alban.brissez@gmail.com>
2026-09-04 16:51:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="baticout-v1.2.0";
+const CACHE="baticout-v1.2.1";
 const SHELL=["./","./index.html","./styles.css","./app.js","./config.js","./manifest.json","./assets/logo-mark.svg","./assets/icon-192.png","./assets/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
